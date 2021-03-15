caec46fb027fa873d636f8159d083ea1be0f5b6b
MatheusAThomaz <thomazamatheus@gmail.com>
2021-03-15 09:23:56 -0300

    Adding logged in page

--- client/login.js
+++ client/login.js
@@ -8,19 +8,26 @@ function login() {
         password: password,
         email: email
     }
-    
+
     $.ajax({
         url: 'https://en4nt6p4m0cu4le.m.pipedream.net',
         type: 'post',
         data: dataBody,
         dataType: 'json',
-        success: function(data) {
-            alert("oiii")
-            console.log(data)
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            console.log(result)
+            
         },
-        error: function(request, error) {
-            alert("Error: ", error)
+        error: function(error) {
+            alert("aqui2")
+            console.log(error)
         }
     })
+
+    window.location.href = "../template/signed-in.html"
+
+    return false;
 
 }
